ea6ac17f78805dc099abe2bed78545ec26b61534
Ioannis Theodosiou <ioannis@info.nl>
2019-10-23 14:59:28 +0200

    back to start

--- solution/screens/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as Details } from './Details';
-export { default as SideBar } from './SideBar';
-export { default as SelectImage } from './SelectImage';
-export { default as Write } from './Write';
