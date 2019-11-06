34387d84dea047b3d8a6d37ea86b93a156e7362d
Ioannis Theodosiou <ioannis@info.nl>
2019-11-06 17:01:49 +0100

    all

--- src/utils/fsMethods.ts
+++ src/utils/fsMethods.ts
@@ -1,40 +1,43 @@
-// import { PermissionsAndroid } from 'react-native';
-// import RNFS from 'react-native-fs';
+import { PermissionsAndroid, Platform } from 'react-native';
+import RNFS from 'react-native-fs';
 
-// /**
-//  * Get permissions to access and read a file from the filesystem.
-//  *
-//  * @returns {Promise<boolean>}
-//  */
-// const path = '/storage/emulated/0/DCIM/test.txt';
+/**
+ * Get permissions to access and read a file from the filesystem.
+ *
+ * @returns {Promise<boolean>}
+ */
+const path = `${RNFS.DocumentDirectoryPath}/test.txt`;
 
-// async function hasFileAccessPermission(): Promise<boolean> {
-//   const result = await PermissionsAndroid.request(
-//     PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
-//   );
-//   return result === PermissionsAndroid.RESULTS.GRANTED;
-// }
+async function hasFileAccessPermission(): Promise<boolean> {
+  if (Platform.OS === 'android') {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+    );
+    return result === PermissionsAndroid.RESULTS.GRANTED;
+  }
+  return true;
+}
 
-// const readFile = async (): Promise<string> => {
-//   const grantStatus = await hasFileAccessPermission();
-//   if (!grantStatus) {
-//     throw new Error('permision denied');
-//   }
-//   const result = await RNFS.readFile(path);
-//   return result;
-// };
+const readFile = async (): Promise<string> => {
+  const grantStatus = await hasFileAccessPermission();
+  if (!grantStatus) {
+    throw new Error('permision denied');
+  }
+  const result = await RNFS.readFile(path);
+  return result;
+};
 
-// const writeFile = async (text: string): Promise<void> => {
-//   // checking read permission
-//   const grantStatus = await hasFileAccessPermission();
-//   if (!grantStatus) {
-//     throw new Error('permision denied');
-//   }
-//   try {
-//     await RNFS.writeFile(path, text, 'utf8');
-//   } catch (error) {
-//     throw Error(error.message);
-//   }
-// };
+const writeFile = async (text: string): Promise<void> => {
+  // checking read permission
+  const grantStatus = await hasFileAccessPermission();
+  if (!grantStatus) {
+    throw new Error('permision denied');
+  }
+  try {
+    await RNFS.writeFile(path, text, 'utf8');
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
 
-// export { readFile, writeFile };
+export { readFile, writeFile };
